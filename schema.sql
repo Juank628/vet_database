b842b7b0c2a376346d3e67fe08f0b49d4f01ee1b
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-20 16:50:22 -0500

    schema, data, and queries completed

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+    id int GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attemps INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
