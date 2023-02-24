07c49dc58b48d4f0ca1a580b0956c24cd739c0d9
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-24 18:55:42 -0500

    project 4 requirements

--- schema.sql
+++ schema.sql
@@ -40,3 +40,32 @@ ALTER TABLE animals ADD CONSTRAINT id PRIMARY KEY(ID);
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id INT, ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD owner_id INT, ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+/*******************************************
+Project 4: add join table for visits
+*******************************************/
+
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations(
+    vet_id INT,
+    species_id INT,
+    PRIMARY KEY(vet_id, species_id),
+    CONSTRAINT fk_vet FOREIGN KEY(vet_id) REFERENCES vets(id),
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits(
+    animal_id INT,
+    vet_id INT,
+    date_of_visit Date,
+    PRIMARY KEY(animal_id, vet_id, date_of_visit),
+    CONSTRAINT fk_animal FOREIGN KEY(animal_id) REFERENCES animals(id),
+    CONSTRAINT fk_vet FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
